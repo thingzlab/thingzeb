23e8d75ae2ffaf78d6aa29cf16c280bc613dac64
thingzlab <thingzlab@gmail.com>
2017-10-26 17:06:09 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Hello Cloudaxis !!!</h1>
+                <h1>Hello Thingz !!!</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in AWS EB.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
